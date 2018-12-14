2e2e8c8b6fe93dc73890baee25323ca3280bf32a
Wise <highwiseman@gmail.com>
2018-12-14 10:45:59 +0300

    Config for HEROKU

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const port = 8383;
+const port = process.env.PORT || 8383;
 
 const chatUser = require('./model/chatuser');
 const Message = require('./model/chat');
